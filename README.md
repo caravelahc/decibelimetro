348e08cbadb5892be726b296191d716da0734e1c
Arthur Mesquita Pickcius <arthur.pickcius@gmail.com>
2019-07-26 10:01:37 -0300

    Potentiometer to decibelimeter

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # Decibelímetro
 
 ## Descrição
-TODO
+Projeto utilizando arduino para medir volume do som no ambiente e avisar caso exceda um certo limite.
 
-### Materiais usados
+## Materiais usados
   - Arduino
   - Decibelímetro
   - Buzzer
+
+## Algoritmo
+As leituras do sensor do decibelímetro oscilam em torno de um valor, variando em amplitude de acordo com a intensidade do som.
+
+Para obter o valor médio são armazenadas diversas leituras anteriores e a média é calculada em cima deles. Para acelerar o cálculo da média é utilizado um vetor com os valores das últimas leituras. O valor novo é somado e o valor a ser sobreescrito no vetor é subtraído da conta.
+
+São calculadas as diferenças entre o valor médio e o valor lido no sensor. É feita uma média dessas diferenças seguindo o mesmo esquema para evitar que um pico sozinho consiga ativar o aviso sonoro.
--- decibelimetro.ino
+++ decibelimetro.ino
@@ -1,69 +1,99 @@
-const int buzzer = 10;
-const int decibelimeter = 0;
-const int MEMO = 100;
+const int BUZZER = 8;
+const int DECIBELIMETER = 0;
+const int MEMORY = 250;
+const int DIF_MEMO = 50;
 
-float volume_signal = 0;
-float volume[MEMO];
+float volume_array[MEMORY];
+float volume_dif_array[DIF_MEMO];
+float volume_dif_sum = 0;
 float volume_sum = 0;
-float volume_mean = 0;
 
-int i = 0;
+int dif_index;
+int index;
   
 void setup() {
   Serial.begin(9600);
-  pinMode(buzzer, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(decibelimeter, INPUT);
+  pinMode(DECIBELIMETER, INPUT);
 
-  for(i=0;i<MEMO;i++){
-    volume[i] = 0;
+  for(dif_index=0;dif_index<DIF_MEMO;dif_index++){
+    volume_dif_array[dif_index] = 0;
+  }
+  for(index=0;index<MEMORY;index++){
+    volume_array[index] = 0;
   }
 }
 
-float get_mean(){
-  if (i >= MEMO-1){
-    i = 0;
+float get_dif_mean(float volume_mean){
+  if (dif_index < DIF_MEMO-1){
+    dif_index++;
   } else {
-    i++;
+    dif_index = 0;
   }
   
-  volume_sum -= volume[i];
-  volume_signal = analogRead(decibelimeter); //*5/1024;
-  volume[i] = volume_signal;
-  volume_sum += volume[i];
+  volume_dif_sum -= volume_dif_array[dif_index];
+  volume_dif_array[dif_index] = analogRead(DECIBELIMETER) - volume_mean;
+  if (volume_dif_array[dif_index] < 0) {
+    volume_dif_array[dif_index] = -volume_dif_array[dif_index];
+  }
+  volume_dif_sum += volume_dif_array[dif_index];
 
-  return volume_sum/MEMO;
+  return volume_dif_sum/DIF_MEMO;
 }
 
-void send_plotter_data(){
-  Serial.print(volume_signal);
+float get_mean() {
+  if (index < MEMORY-1){
+    index++;
+  } else {
+    index = 0;
+  }
+  
+  volume_sum -= volume_array[index];
+  volume_array[index] = analogRead(DECIBELIMETER);
+  volume_sum += volume_array[index];
+
+  return volume_sum/MEMORY;
+}
+
+void send_plotter_data(float volume_dif_mean, float volume_mean){
+  Serial.print(analogRead(DECIBELIMETER));
+  Serial.print(",");
+  Serial.print(volume_dif_mean);
   Serial.print(",");
   Serial.println(volume_mean);
 }
 
-void reset_volume_memo() {
-  int j;
-  for (j = 0; j < MEMO; j++){
-    volume[j] = 0;
-  }
+void reset_memory() {
+  int i;
   volume_sum = 0;
+  volume_dif_sum = 0;
+  for (i = 0; i < MEMORY; i++){
+    volume_array[i] = analogRead(DECIBELIMETER);
+    volume_sum += volume_array[i];
+    if (i >= MEMORY - DIF_MEMO) {
+      volume_dif_array[i - (MEMORY - DIF_MEMO)] = 0;
+    }
+  }
+  volume_dif_sum += 0;
 }
 
 void loop() {
-  volume_mean = get_mean();
+  float volume_mean = get_mean();
+  float volume_dif_mean = get_dif_mean(volume_mean);
   
-  send_plotter_data();
+  send_plotter_data(volume_dif_mean, volume_mean);
 
-  if (volume_mean > 500) {
-    tone(buzzer, 1500);
+  if (volume_dif_mean > 50) {
+    tone(BUZZER, 1500);
     digitalWrite(LED_BUILTIN, HIGH);
     delay(500);
-    noTone(buzzer);
+    noTone(BUZZER);
     delay(5000);
-    reset_volume_memo();
+    reset_memory();
   }
   else {
     digitalWrite(LED_BUILTIN, LOW);
-    noTone(buzzer);
+    noTone(BUZZER);
   }
 }
